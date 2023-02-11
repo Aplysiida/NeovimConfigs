41eb63e5896d83f3a856bb130ba79bae47a72540
Aplysiida <61487136+Aplysiida@users.noreply.github.com>
2023-02-11 23:58:57 +1300

    Added load html command

--- webdev/init.lua
+++ webdev/init.lua
@@ -1,3 +1,19 @@
 vim.opt.number = true
 
+function LoadPageToBrowser()
+	local filepath = vim.fn.expand("%:p")
+	io.popen("start firefox " .. filepath)
+end
+
+function noremap(mode, lhs, rhs, opts) 
+	local options = { noremap = true }
+    	if opts then
+        	options = vim.tbl_extend("force", options, opts)
+    	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.api.nvim_create_user_command("LoadPageToBrowser", function() LoadPageToBrowser() end, {nargs = 0, desc = 'Load html to browser'})
+noremap('n', "<F2>", ":LoadPageToBrowser", {})
+
 require ("webdev_plugins")
