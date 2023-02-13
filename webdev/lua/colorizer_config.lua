8f89e9ae9a2f29c85b825c21fc258f71f490b857
VicsStuff <61487136+Aplysiida@users.noreply.github.com>
2023-02-13 20:05:46 +1300

    Added colorizer

---
+++ webdev/lua/colorizer_config.lua
@@ -0,0 +1,6 @@
+colorizer_config = require("colorizer")
+
+colorizer_config.setup({
+	'*';
+	css, html = { names = true; css = true; };
+})
--- webdev/lua/webdev_plugins.lua
+++ webdev/lua/webdev_plugins.lua
@@ -7,10 +7,11 @@ vim.call("plug#begin", plug_path)
 Plug "williamboman/mason.nvim"
 Plug "williamboman/mason-lspconfig.nvim"
 Plug "neovim/nvim-lspconfig"
+Plug "nvim-treesitter/nvim-treesitter"
 Plug "preservim/nerdtree"
 Plug "Yggdroot/indentLine"
-Plug "nvim-treesitter/nvim-treesitter"
 Plug "mattn/emmet-vim"
+Plug "norcalli/nvim-colorizer.lua"
 Plug "morhetz/gruvbox"
 
 vim.call("plug#end")
@@ -28,6 +29,8 @@ vim.cmd [[ cd ~/Documents/ ]]
 if(vim.fn.has("termguicolors")) then 
 	vim.opt.termguicolors = true	-- enable true colors support
 end
+
+require("colorizer_config")
 
 vim.g.gruvboxcolor = "dark"
 vim.cmd [[ colorscheme gruvbox ]]
